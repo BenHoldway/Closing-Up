c588ed38ec984ea0e284649239bbb3df891bbc9d
BenHoldway03 <benholdway03@gmail.com>
2024-04-26 14:37:55 +0100

    Interactable Spawning Fixed

--- Assets/Scripts/Interactables/SpawnInteractables.cs
+++ Assets/Scripts/Interactables/SpawnInteractables.cs
@@ -9,42 +9,61 @@ public class SpawnInteractables : MonoBehaviour
 
     Collider2D[] cols = new Collider2D[1];
 
+    Vector2 spawnPoint = Vector2.zero;
+    bool isAttemptingToSpawn;
+
     // Start is called before the first frame update
     void Awake()
     {
+        isAttemptingToSpawn = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonUp(0)) 
+        if (Input.GetMouseButtonDown(0) || isAttemptingToSpawn)
         {
-            Spawn();
+            bool hasSpawned = false;
+            isAttemptingToSpawn = true;
+
+            if (!hasSpawned)
+                hasSpawned = Spawn();
+
+            if (hasSpawned)
+                SetInteractablePos();
         }
     }
 
-    void Spawn() 
+    bool Spawn() 
     { 
-        Vector2 spawnPoint = GetRandomPoint();
+        spawnPoint = GetRandomPoint();
 
         int numFound = Physics2D.OverlapCircleNonAlloc(spawnPoint, 0.01f, cols, floorLayer);
 
         if (numFound == 0)
-            Spawn();
+            return false;
         else
-        {
-            GameObject interactable = Instantiate(taskInteractable);
-            interactable.transform.position = new Vector3(spawnPoint.x, spawnPoint.y, 0);
-        }
+            return true;
     }
 
     Vector2 GetRandomPoint()
     {
         Bounds bound = floor.GetComponent<CompositeCollider2D>().bounds;
 
-        float spawnX = Random.Range(-bound.extents.x, bound.extents.x);
-        float spawnY = Random.Range(-bound.extents.y, bound.extents.y);
+
+        float spawnX = Random.Range(floor.transform.position.x - bound.extents.x, floor.transform.position.x + bound.extents.x);
+        float spawnY = Random.Range(floor.transform.position.y - bound.extents.y, floor.transform.position.y + bound.extents.y);
+        print($"X: {Random.Range(floor.transform.position.x - bound.extents.x, floor.transform.position.x + bound.extents.x)}, Y: {Random.Range(floor.transform.position.y - bound.extents.y, floor.transform.position.y + bound.extents.y)}");
+        print($"Spawnpoint: {new Vector2(spawnX, spawnY)}");
 
         return new Vector2(spawnX, spawnY); 
+    }
+
+    void SetInteractablePos()
+    {
+        GameObject interactable = Instantiate(taskInteractable);
+        interactable.transform.position = new Vector3(spawnPoint.x, spawnPoint.y, 0);
+
+        isAttemptingToSpawn = false;
     }
 }
